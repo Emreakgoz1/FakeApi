23dcf9a7b4b6c5bd0aaab76585d64a8639955bd4
Emreakgoz1 <baranemreakgoz@gmail.com>
2023-05-04 02:39:29 +0300

    css improvement

--- script.js
+++ script.js
@@ -6,28 +6,40 @@ async function getText(url) {
   const y = await x.json();
   return y;
 }
-
 getText(url).then((data) => {
-  const products = data; // verileri bir değişkene atama
+  const products = data;
   const divelement = document.querySelector("#div");
-  const divbutton = document.querySelector("#increase");
-  const divbutton2 = document.querySelector("#decrease");
-  products
-    .sort((a, b) => {
+  const divbutton = document.querySelector(".increase");
+  const divbutton2 = document.querySelector(".decrease");
+  const sortIncrease = () => {
+    const sortedProducts = products.sort((a, b) => {
       return a.price - b.price;
-    })
-    .map((item, index) => {
+    });
+    showProducts(sortedProducts);
+  };
+  const sortDecrease = () => {
+    const sortedProducts = products.sort((a, b) => {
+      return b.price - a.price;
+    });
+    showProducts(sortedProducts);
+  };
+  const showProducts = () => {
+    divelement.innerHTML = "";
+    products.map((item, index) => {
       if (index < 9) {
-        //decrease increase buton koy
-        //price göre sırala
-        //divelement.innerHTML += `<div id="card"><h2 id="cardtitle">${item.title}</h2><p id="cardprice">${item.price}</p><p id="carddescription">${item.description}</p></div>`;
         divelement.innerHTML += `
-        <div class="col-12 col-md-6 col-xl-4">
-        <div id="card"><h2 id="cardtitle">${item.title}</h2><p id="cardprice">${item.price}</p><p id="carddescription">${item.description}</p></div>
-        </div>
+          <div class="col-12 col-md-6 col-xl-4">
+            <div id="card"><h2 id="cardtitle">${item.title}</h2><p id="cardprice">${item.price}</p><p id="carddescription">${item.description}</p></div>
+          </div>
         `;
       }
-      // divelement.innerHTML += `<li></li>`;
-      // divelement.innerHTML += `<li></li>`;
     });
+  };
+  sortIncrease();
+  divbutton.addEventListener("click", () => {
+    sortIncrease();
+  });
+  divbutton2.addEventListener("click", () => {
+    sortDecrease();
+  });
 });
